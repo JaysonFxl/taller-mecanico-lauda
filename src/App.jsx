b53d58590ececa4dbe14f4b1d27e169061b08f27
JaysonFxl <jayson.flores@inacapmail.cl>
2023-07-09 00:20:22 -0400

    Cambios efectos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
 import { Carousel } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import Zoom from 'react-reveal/Zoom';
@@ -82,30 +83,90 @@ function Inicio() {
 }
 
 function ReservarCita() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+    Swal.fire(
+      'Formulario enviado',
+      'Tu formulario ha sido enviado con éxito',
+      'success'
+    );
+  };
+  const handleLimpiar = () => {
+    reset(); // Limpia los campos del formulario
+    Swal.fire(
+      'Formulario limpiado',
+      'Todos los campos del formulario han sido limpiados',
+      'success'
+    );
+  };
 
   return (
-    <div className="container py-5">
-      <h1 className="display-4 mb-4 text-center">Reservar Cita</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-3">
-          <input {...register("nombre", { required: true })} placeholder="Nombre" className="form-control" />
-          {errors.nombre && <div className="text-danger">Este campo es requerido</div>}
+    <div className="container py-5 mt-5">
+      <Fade bottom>
+        <div className="card">
+          <div className="card-body">
+            <h1 className="display-4 mb-4 text-center">Reservar Cita</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="row">
+          <div className="col-md-6 mb-3">
+            <input {...register("rut", { required: true })} placeholder="RUT" className="form-control" />
+            {errors.rut && <div className="text-danger">Este campo es requerido</div>}
+          </div>
+          <div className="col-md-6 mb-3">
+            <input {...register("nombre", { required: true })} placeholder="Nombre" className="form-control" />
+            {errors.nombre && <div className="text-danger">Este campo es requerido</div>}
+          </div>
         </div>
-        
-        <div className="mb-3">
-          <input {...register("email", { required: true })} placeholder="Email" className="form-control" />
-          {errors.email && <div className="text-danger">Este campo es requerido</div>}
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <input {...register("apellido", { required: true })} placeholder="Apellido" className="form-control" />
+            {errors.apellido && <div className="text-danger">Este campo es requerido</div>}
+          </div>
+          <div className="col-md-6 mb-3">
+            <input {...register("email", { required: true })} placeholder="Email" className="form-control" />
+            {errors.email && <div className="text-danger">Este campo es requerido</div>}
+          </div>
         </div>
-        
-        <div className="mb-3">
-          <input {...register("fecha", { required: true })} type="date" className="form-control" />
-          {errors.fecha && <div className="text-danger">Este campo es requerido</div>}
+        <div className="row">
+        <div className="col-md-6 mb-3">
+          <div className="input-group">
+            <span className="input-group-text" id="basic-addon1">+56</span>
+            <input {...register("telefono", { required: true, pattern: /^[0-9]*$/ })} placeholder="Teléfono" className="form-control" />
+          </div>
+          {errors.telefono && <div className="text-danger">Este campo es requerido y solo debe contener números</div>}
         </div>
-        
-        <input type="submit" className="btn btn-warning" />
-      </form>
+          <div className="col-md-6 mb-3">
+            <select {...register("motivo", { required: true })} className="form-control">
+              <option value="">-- Seleccione un motivo --</option>
+              <option value="reparacion">Reparación</option>
+              <option value="mantenimiento">Mantenimiento</option>
+            </select>
+            {errors.motivo && <div className="text-danger">Este campo es requerido</div>}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <input {...register("patente", { required: true })} placeholder="Patente del vehículo" className="form-control" />
+            {errors.patente && <div className="text-danger">Este campo es requerido</div>}
+          </div>
+          <div className="col-md-6 mb-3">
+            <input {...register("modelo", { required: true })} placeholder="Modelo del vehículo" className="form-control" />
+            {errors.modelo && <div className="text-danger">Este campo es requerido</div>}
+          </div>
+        </div>
+        <div className="mb-3">
+          <textarea {...register("detalles", { required: true })} placeholder="Detalles de la consulta" className="form-control" rows="3"></textarea>
+          {errors.detalles && <div className="text-danger">Este campo es requerido</div>}
+        </div>
+              <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                <input type="submit" className="btn btn-warning" value="Enviar" />
+                <button type="button" className="btn btn-danger" onClick={handleLimpiar}>Limpiar</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </Fade>
     </div>
   );
 }
@@ -144,11 +205,43 @@ function Servicios() {
 
 function Contacto() {
   return (
-    <div className="container text-center py-5">
-      <h1 className="display-4 mb-4">Contacto</h1>
-      <p className="lead">Número de contacto: (123) 456-7890</p>
-      <p className="lead">Dirección: 123 Calle Principal, Ciudad, Estado, Código Postal</p>
-      <p className="lead">Email: taller@nikilauda.com</p>
+    <div className="container py-5 mt-5">
+      <h1 className="display-4 mb-4 text-center">Contacto</h1>
+      <div className="row justify-content-center">
+        <Fade left>
+          <div className="col-md-6 col-lg-4 mb-4">
+            <div className="card bg-dark text-warning h-100">
+              <div className="card-body text-center">
+                <i className="fas fa-phone fa-3x mb-3"></i>
+                <h5 className="card-title">Número de contacto</h5>
+                <p className="card-text">(123) 456-7890</p>
+              </div>
+            </div>
+          </div>
+        </Fade>
+        <Fade bottom>
+          <div className="col-md-6 col-lg-4 mb-4">
+            <div className="card bg-dark text-warning h-100">
+              <div className="card-body text-center">
+                <i className="fas fa-map-marker-alt fa-3x mb-3"></i>
+                <h5 className="card-title">Dirección</h5>
+                <p className="card-text">123 Calle Principal, Ciudad, Estado, Código Postal</p>
+              </div>
+            </div>
+          </div>
+        </Fade>
+        <Fade right>
+          <div className="col-md-6 col-lg-4 mb-4">
+            <div className="card bg-dark text-warning h-100">
+              <div className="card-body text-center">
+                <i className="fas fa-envelope fa-3x mb-3"></i>
+                <h5 className="card-title">Email</h5>
+                <p className="card-text">taller@nikilauda.com</p>
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </div>
     </div>
   );
 }
